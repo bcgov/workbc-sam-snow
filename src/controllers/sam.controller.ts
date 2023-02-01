314c015ad8c4e641f2b31aa6644940a7757993b3
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2023-02-01 09:22:58 -0800

    linting

--- src/controllers/sam.controller.ts
+++ src/controllers/sam.controller.ts
@@ -7,8 +7,12 @@ export const getPermissions = async (req: express.Request, res: express.Response
     try {
         const user = await samService.getUser(req.params.guid, false)
         // console.log(user)
-        const accessEnded = (moment(user.EndDate).utc().diff(moment().utc()) < 0)
-        const hasSNOWAccess = user.Properties.some((props: any) => props.SecurityRole.ApplicationCode === "SARA" && (moment(props.EndDate).isValid() ? ((moment(props.EndDate).utc().diff(moment().utc())) < 0) : true))
+        const accessEnded = moment(user.EndDate).utc().diff(moment().utc()) < 0
+        const hasSNOWAccess = user.Properties.some(
+            (props: any) =>
+                props.SecurityRole.ApplicationCode === "SARA" &&
+                (moment(props.EndDate).isValid() ? moment(props.EndDate).utc().diff(moment().utc()) < 0 : true)
+        )
 
         user.SNOWAccess = !accessEnded && hasSNOWAccess
         delete user.StartDate
@@ -32,8 +36,12 @@ export const getAll = async (req: express.Request, res: express.Response) => {
         const filteredUsers = users.filter((item: any) => item.Properties.length > 0)
         // let organizations: any[]= []
         filteredUsers.forEach((u: any) => {
-            const accessEnded = (moment(u.EndDate).utc().diff(moment().utc()) < 0)
-            const hasSNOWAccess = u.Properties.some((props: any) => props.SecurityRole.ApplicationCode === "SARA" && (moment(props.EndDate).isValid() ? ((moment(props.EndDate).utc().diff(moment().utc())) < 0) : true))
+            const accessEnded = moment(u.EndDate).utc().diff(moment().utc()) < 0
+            const hasSNOWAccess = u.Properties.some(
+                (props: any) =>
+                    props.SecurityRole.ApplicationCode === "SARA" &&
+                    (moment(props.EndDate).isValid() ? moment(props.EndDate).utc().diff(moment().utc()) < 0 : true)
+            )
             /*
             const org = u.Organization || ""
             if (!(organizations.indexOf(org) > -1)){
