b0d7522104237dfb0026f698a485543b7aa48e29
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2023-02-01 11:15:39 -0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ import helmet from "helmet"
 import bodyParser from "body-parser"
 import basicAuth from "express-basic-auth"
 import * as sam from "./routes/sam.route"
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 const morgan = require("morgan")
 
 const APP_USER: string = process.env.APP_USER!
 const APP_PASS: string = process.env.APP_PASS!
 const users: { [k: string]: any } = {}
 users[APP_USER] = APP_PASS
-console.log(users)
+// console.log(users)
 
 export const app = express()
 
