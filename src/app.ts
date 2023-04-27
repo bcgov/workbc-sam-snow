48dd1cff44c73666af946ec6414fda2aeea34e6a
npham49 <brian.d.pham@gov.bc.ca>
2023-04-27 15:59:09 -0700

    setting up local user imports

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express"
 import helmet from "helmet"
 import bodyParser from "body-parser"
 // import basicAuth from "express-basic-auth"
-import jwt from "jsonwebtoken"
+// import jwt from "jsonwebtoken"
 import * as sam from "./routes/sam.route"
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 const morgan = require("morgan")
@@ -37,28 +37,28 @@ app.use(
     })
 )
 
-const validate = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    try {
-        if (!req.headers.authorization) {
-            return res.status(403).send("Unauthorized")
-        }
-        if (!req.headers.authorization.includes("Bearer")) {
-            return res.status(403).send("Unauthorized")
-        }
-        // else some kind of token is present
-        const token = req.headers.authorization.split(" ")[1]
-        const verify = jwt.verify(token, process.env.APP_PASS || "", { audience: process.env.APP_USER || "" })
-        if (verify) {
-            return next()
-        }
-        return res.status(403).send("Unauthorized")
-    } catch (error: any) {
-        console.log(error)
-        return res.status(500).send("Internal Server Error")
-    }
-}
+// const validate = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//     try {
+//         if (!req.headers.authorization) {
+//             return res.status(403).send("Unauthorized")
+//         }
+//         if (!req.headers.authorization.includes("Bearer")) {
+//             return res.status(403).send("Unauthorized")
+//         }
+//         // else some kind of token is present
+//         const token = req.headers.authorization.split(" ")[1]
+//         const verify = jwt.verify(token, process.env.APP_PASS || "", { audience: process.env.APP_USER || "" })
+//         if (verify) {
+//             return next()
+//         }
+//         return res.status(403).send("Unauthorized")
+//     } catch (error: any) {
+//         console.log(error)
+//         return res.status(500).send("Internal Server Error")
+//     }
+// }
 
-app.use("/sam", validate, sam.router)
+app.use("/sam", sam.router)
 
 const port = process.env.PORT || "8000"
 app.listen(port, () => {
